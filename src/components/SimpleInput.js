c4378736f2df8b77b04c642c37845d4e0a432acf
Trey Waller <tlwaller99@gmail.com>
2022-03-02 13:48:42 -0600

    formatting

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,23 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const [name, setName] = useState("");
+
+  const inputHandler = (e) => {
+    setName(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    console.log(name);
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={submitHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" onChange={inputHandler} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
